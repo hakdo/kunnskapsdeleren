adf9c47fa635d0a220f14935c3ede805fa9d6d9e
Håkon Olsen <hakdo@outlook.com>
2016-12-03 13:23:02 +0100

    lagt til admin for vestrerosten

--- vestrerosten/models.py
+++ vestrerosten/models.py
@@ -6,3 +6,6 @@ class Underskrifter(models.Model):
     navn = models.CharField(max_length=255)
     adresse = models.CharField(max_length=255)
     kommentar = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.navn+' '+self.adresse
